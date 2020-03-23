f2f7c27a0b77ee4961cce115e945f52966309d37
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-03-23 14:29:18 -0500

    Add Router and routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageHeader/>
+      <Router>
+        <Switch>
+          <Route exact path= '/' component= {Home} />
+          <Route exact path= '/about' component= {About} />
+          <Route exact path= '/admin/newmember' component= {AddMemberForm} />
+          <Route exact path= '/member/myheraldry' component= {HeraldryForm} />
+        </Switch>
+      </Router>
     </div>
   );
 }
