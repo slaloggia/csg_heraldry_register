675c1f74813231a9941094514e6e8fd3303539c8
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-03-25 19:07:36 -0500

    Create AddMemberForm component with state hooks and submit function

--- src/components/AddMemberForm.js
+++ src/components/AddMemberForm.js
@@ -1,5 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function AddMemberForm() {
-    return <h1>Add Member</h1>
+    const [name, setName] = useState('')
+    const [guildName, setGuildName] = useState('')
+    const [joined, setJoined] = useState('')
+    const [rank, setRank] = useState('')
+    const [focus, setFocus] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        let memberObj = {
+            name: name,
+            guild_name: guildName,
+            joined: parseInt(joined),
+            rank: rank,
+            focus: focus
+        }
+        const reqObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(memberObj)
+        }
+
+        fetch('http://localhost:5000/members', reqObj)
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    }
+    
+
+    return (
+        <div>
+            <h1>Add Member</h1>
+            <form onSubmit= {handleSubmit}>
+                <label>Name: </label>
+                <input value={name} placeholder="Full Name" onChange={e => setName(e.target.value)} />
+                <br/>
+                <label>Guild Name: </label>
+                <input value={guildName} placeholder="e.g. Scooter, HP, etc." onChange={e => setGuildName(e.target.value)}/>
+                <br/>
+                <label>Year Joined: </label>
+                <select value={joined} onChange={e => setJoined(e.target.value)} >
+                    {listYearOptions()}
+                </select>
+                <br/>
+                <label>Rank: </label>
+                <select value={rank} onChange={e => setRank(e.target.value)} >
+                    <option value="Scholar">Scholar</option>
+                    <option value="Free Scholar">Free Scholar</option>
+                    <option value="Provost" >Provost</option>
+                    <option value="Dean" >Greg</option>
+                </select>
+                <br/>
+                <label>Focus: </label>
+                <select value={focus} onChange={e => setFocus(e.target.value)} >
+                    <option value="Renaissance">Renaissance</option>
+                    <option value="Medieval">Medieval</option>
+                </select>
+                <br/>
+                <input type="Submit" />
+            </form>
+        </div>
+
+    )
+}
+
+function listYearOptions() {
+    const today = new Date()
+    const currentYear = today.getFullYear()
+    let y = 1999
+    let years =[]
+    while (y <= currentYear) {
+       years.push(y)
+       y++
+    }
+
+    return years.map(yr => <option value={yr} key={yr}>{yr}</option>)
+
 }
--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function CardFront(member) {
-    console.log(member)
     return (
         <div>
             <img src={require('../assets/shield.png')} alt="blank shield" className="front-img"/>
