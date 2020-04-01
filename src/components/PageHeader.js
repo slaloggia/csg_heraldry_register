61a649b9716ae0fd093cdfd1850ce2e8d612c98b
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-03-31 20:23:42 -0500

    Create the static About page

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -7,12 +7,12 @@ export default function PageHeader() {
             <img src={require('../assets/csglogo.jpg')} id="logo" alt= "CSG logo" />
             <h1>Chicago Swordplay Guild</h1>
             <h2>Heraldic Register</h2>
-            <div className="links-menu">
+            <nav className="links-menu">
                 <a href="/" >Home</a><br/>
                 <a href="/about">About</a><br/>
                 <a href="/member/myheraldry" >Add Your Heraldry</a><br/>
                 <a href="/admin/newmember">Admin</a>
-            </div>
+            </nav>
             </span>
         </div>)
 };
--- src/containers/About.js
+++ src/containers/About.js
@@ -1,5 +1,72 @@
 import React from 'react'
 
 export default function About() {
-    return <h2>About</h2>
+    return (
+        <div id="about">
+            <h2>About Personal Heraldry</h2>
+                <p>
+                Scholars and Free Scholars of the Chicago Swordplay Guild are permitted to create a heraldic shield design, 
+                for display within the Guild Book, as part of their advancement to the rank of Scholar or Free Scholar. 
+                This heraldic shield must fit within the guidelines of heraldry laid out for advancing Guilders. 
+                Once designed by the Scholar, the shield will be illuminated by one of the Guild book artists: Victor Bayona, Erin Fitzgerald, Heather Hilchey, Rebecca Smith. 
+                Additionally, those advancing to the rank of Free Scholar or Provost will be allowed to create a full Heraldic Achievement 
+                with their Scholar shield advanced to include a motto, supporters, helm, crest, and mantle. 
+                Some additional personalized elements will be permitted for the rank of Provost. 
+                <br/>
+                Students are responsible for researching and selecting the elements of their Coat of Arms. 
+                General resources are provided in the Guild Handbook.
+                </p>
+            <br/>
+            <hr/>
+            <h3>Scholars</h3>
+                <p>
+                Upon obtaining the rank of Scholar, a Scholar may fashion a shield of their own design to be memorialized in the Guild Book. 
+                This shield must meet heraldic and reproductive standards, for consistency and re-creation. 
+                When creating a shield, the Scholar must, at a minimum, choose a field color(s) and a division of the field or Ordinary or Charge. 
+                Depending on the complexity desired the shield may include all of the above.<br/>
+                The following links provide some generic explanations of the shield and associated elements.  
+                    <ul>
+                        <li><a href="https://en.wikipedia.org/wiki/Escutcheon_(heraldry)" target="_blank">Escutcheon</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Blazon " target="blank">Blazon</a></li>
+                    </ul>
+                </p>
+                <h4>Guidelines</h4>
+                    <ul>
+                        <li>Shield Shape – A standard “heater style” shield shape will be utilized, in keeping with the Guild’s shield 
+                            style.
+                        </li>
+                        <li><a href="https://en.wikipedia.org/wiki/Tincture_(heraldry)" target="_blank">Field Colors – (Tincture)</a>– Metals: Silver (white), Gold (yellow), Red, Maroon, Orange, Blue, Green, Purple, Black, Brown, Grey, Sky Blue - Patterns/Furs  </li>
+                        <li><a href="https://en.wikipedia.org/wiki/Division_of_the_field" target="_blank">Divisions of the Field</a> </li>
+                        <li><a href="https://en.wikipedia.org/wiki/Charge_(heraldry)" target="_blank">Charges</a> – A Charge is any device that occupies the field and can be an Ordinary (or geometric pattern), a common charge, a figure, animal, plant, object, or a grouping of these 
+                            <ul>
+                                <li><a href="https://en.wikipedia.org/wiki/Ordinary_(heraldry)" target="_blank">Ordinaries </a>can stand alone or can be used to divide the field </li>
+                                <li><a href="https://en.wikipedia.org/wiki/List_of_heraldic_charges" target="_blank">Common charges</a> </li>
+                                <li><a href="https://en.wikipedia.org/wiki/Attitude_(heraldry)" target="_blank">Orientation or “Attitude” of Charges</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <p>
+                        Your final design must be simple enough to be reproducible.
+                        It should be visually clear regardless of the size of the shield or distance of viewing. 
+                        Heraldic rules for the above, must be adhered to. Modern objects as charges are not acceptable for the Coat of Arms project.
+                    </p>
+                <h4>Process</h4>
+                    <ol>
+                        <li>Within 30 of playing your prize, you will be added to this page</li>
+                        <li>Design your shield
+                            <ul>
+                                <li>Choose colors, tincture, divisions of the field, and charge</li>
+                                <li>Draft the design - reach out to a Guild book artist for advice if 
+                                you are having difficulty with the composition
+                                <li><a href="https://drawshield.net/index.html" target="_blank">DrawShield</a> may also be a helpful resource for learning blazonry and creating a digital version of your shield </li>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Have your design approved by the Dean and a Guild book artist</li>
+                        <li>When your design is approved, you will get a link to the form to add your Coat of Arms to the digital registry</li>
+                    </ol>
+
+            <hr/>
+        </div>
+    )
 }
