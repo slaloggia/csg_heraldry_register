74296e882dc7583c0024ce036891ad872816930f
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-04-30 16:59:58 -0500

    Create a responsive mobile layout for the page header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -4,7 +4,7 @@ export default function PageHeader() {
     return (
         <div id="page-header">
             <div id="logo">
-                <img src={require('../assets/csglogo.jpg')} id="logo-img" alt= "CSG logo" />
+                <img src={require('../assets/csglogo2.png')} id="logo-img" alt= "CSG logo" />
             </div>
             <span id="title">
                 <h1>Chicago Swordplay Guild</h1>
